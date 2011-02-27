30ac1069f2e326f3954058b322abb162b4febf36
addyrookie <adityadixit06@gmail.com>
2011-02-27 22:36:54 +0530

    First commit

---
+++ tweet-groups/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TweetGroups::Application.config.secret_token = '42cc622255910aa2c2a8b59cd5c25fabadea0cec3c6f3ca9a68c72b6bd5825fa34cc897b0f98c413c80d5d1f9d95dae9db4c7450fedcb322014abcb1cb0fc9a3'
